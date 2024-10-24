e41f6210df5043696a92cfa43bb2cb12b8f1da1c
ai-ngabo <a.ngabo@alustudent.com>
2024-10-24 15:13:39 +0200

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -44,11 +44,13 @@ def div(a, b):
 
 
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
 
-print("{} + {} = {}".format(a, b, result_add))
-print("{} - {} = {}".format(a, b, result_sub))
-print("{} * {} = {}".format(a, b, result_mul))
-print("{} / {} = {}".format(a, b, result_div))
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))
