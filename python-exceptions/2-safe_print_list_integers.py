2f943a312606ca53742640401689e4570847a853
ai-ngabo <a.ngabo@alustudent.com>
2024-11-07 20:15:03 +0200

    2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb_print = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(nb_print))
+            nb_print += 1
+        except (ValueError, TypeError):
+            pass
+        print()
+        return nb_print
