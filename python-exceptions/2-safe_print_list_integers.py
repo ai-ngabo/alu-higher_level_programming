e57df8ec4735ccf6510fe44c2478f40a9134cba3
ai-ngabo <a.ngabo@alustudent.com>
2024-11-07 20:16:26 +0200

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_print += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return nb_print
+    print()
+    return nb_print
