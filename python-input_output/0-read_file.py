2d212e99e991f2b43be7d95e54a5c8f9ac6a9a77
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 12:00:59 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
-"""
-file reader module
-This module provides a function to read a text file and print out its content
-"""
 #!/usr/bin/python3
+"""This is  a file reader module to a filenem (utf-8) and print it to stdout"""
+
+
 def read_file(filename=""):
-    """
-    This function hold a variable name file name means a file to be read
-    """
-    with open(filename, 'r', encoding='utf-8') as myFile:
-        line = myFile.read()
-        print(line)
+    """This function hold a variable name file name means a file to be read"""
+    with open(filename, encoding='utf-8') as myFile:
+        print(myFile.read(), end='')
