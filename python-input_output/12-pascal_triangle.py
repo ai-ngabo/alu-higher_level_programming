ecc20fa5bffe76fda2f4a912106c8e1a96339444
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 23:28:06 +0200

    finalizing

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""I'm not allowed to google anything hahah!"""
+"""This module implements pasals triangle"""
 
 
 def pascal_triangle(n):
-    """Defining Pascal Triangle Function"""
+    """Returns a list representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
     pascal_triangle = [[1]]
@@ -11,8 +11,8 @@ def pascal_triangle(n):
     while len(pascal_triangle) < n:
         new_row = [1]
         base = pascal_triangle[-1]
-        
-        for i in range(len(base) -1):
+
+        for i in range(len(base) - 1):
             new_row.append(base[i] + base[i + 1])
         new_row.append(1)
         pascal_triangle.append(new_row)
