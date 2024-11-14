948210183273fb82ed8650448921c71c54890979
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 12:08:30 +0200

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""file overwritting or creating if not exists!"""
+"""writes a string to a file and returns the num of chars written:"""
 
 
 def write_file(filename="", text=""):
-    """opening file with utf-8 and overwrite"""
-    with open(filename, 'w', enconding='utf-8') as myFile:
-        return myFile.write(text)
+    """Write a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
