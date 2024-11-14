2b57cd128072c0e157f6270d1bb1d7acb2164297
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 11:44:29 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
 def read_file(filename=""):
-    with open('0-main.py', encoding='utf-8') af myFile:
+    with open(filename, 'r', encoding='utf-8') as myFile:
         line = myFile.read()
-        print(line, end='')
-
-read_file()
+        print(line)
