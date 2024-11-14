54af331319a063e646401f4f5ee18e0429c1af15
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 12:26:43 +0200

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""returning json string"""
 import json
-def to_json_string(my_obj):
-    data = json.loads(my_dict)
-to_json_string()
 
+
+def to_json_string(my_obj):
+    """
+    function to return a json  with object: string
+    """
+    data = json.dumps(my_obj)
+    return data
