413a968d344db5006a260ee9d98160b3fbd17bd5
ai-ngabo <a.ngabo@alustudent.com>
2024-11-14 12:29:59 +0200

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Turning JSON string into JSON object--- diserialization"""
 import json
+
+
 def from_json_string(my_str):
-    data1 = json.load(e)
-    print(e)
-from_json_string()
+    """
+    function to return diserialized string into object
+    """
+    data1 = json.loads(my_str)
+    return data1
