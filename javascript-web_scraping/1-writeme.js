39f53149e26afa5f60ace79e2f584a24dee200d7
ai-ngabo <a.ngabo@alustudent.com>
2025-02-07 19:33:00 +0200

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], 'process.argv[3]', 'utf-8', function(err, data) {
-  err && console.log(err);
-  const content = data;
-  console.log(content);
-}) 
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+}); 
