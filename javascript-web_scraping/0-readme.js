1055433262e7c8ec2502e7b5b6ff284d98239b48
ai-ngabo <a.ngabo@alustudent.com>
2025-02-07 19:35:47 +0200

    added new features!

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs'); 
+const fs = require('fs');
 
 fs.readFile(process.argv[2], function (err, data) {
-  err && console.log(err); 
+  err && console.log(err);
   const content = data;
   console.log(content.toString());
 });
