6c90e062819d10302be30314c9a0e43835bf653a
ai-ngabo <a.ngabo@alustudent.com>
2025-02-07 19:11:33 +0200

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs'); //This line imports file system module to handle file operations
+const fs = require('fs'); 
 
 fs.readFile(process.argv[2], function(err, data) {
-  err && console.log(err); // this logs any error to the console
+  err && console.log(err); 
   const content = data;
   console.log(content.toString());
 });
